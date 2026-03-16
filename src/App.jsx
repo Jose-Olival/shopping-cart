765a0a16ec750cdc7eb6af40c9baaae2b848cd2b
Jose Olival <joseandresolival@gmail.com>
2026-03-16 15:37:45 -0400

    productsFilter function created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from 'react'
 import { Products } from './componets/Products'
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
 
 
 function App( ) {
+  const [products, setProducts] = useState(initialProducts)
+  const [filter, setFilter] = useState({
+    category: 'all',
+    minPrice: 0,
+    maxPrice: -1
+  })
+
+  const productsFilter = (products) => {
+    return products.filter((product) => {
+      return filter.minPrice <= product.price &&
+      (filter.maxPrice === -1 || filter.maxPrice >= product.price ) &&
+      (filter.category === 'all' || filter.category === product.category )
+    })
+  }
+
+  const filteredProducts = productsFilter(products)
+
   return (
     <>
     <h1>Tienda</h1>
-        <Products products={products}/>
+        <Products products={filteredProducts}/>
     </>
   )
 }
