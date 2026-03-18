46d52667e6eeab75ff40d7c08daf42961dca6bcc
Jose Olival <joseandresolival@gmail.com>
2026-03-18 15:30:29 -0400

    spelling mistake fix

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { cartContext } from '../context/cart.jsx'
 
 export function useCart() {
-  const context = useContext(cart)
+  const context = useContext(cartContext)
 
   return context
 }
